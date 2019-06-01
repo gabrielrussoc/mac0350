3d4b74251d64e08a39dbe00710853807423da9c4
Gabriel Russo <gabrielrcarmo@gmail.com>
2019-06-01 12:24:11 -0300

    simplficando alguns reads

--- ep2/DML_RETRIEVAL_GROUP.sql
+++ ep2/DML_RETRIEVAL_GROUP.sql
@@ -16,15 +16,10 @@ LANGUAGE plpgsql;
 CREATE OR REPLACE FUNCTION get_curriculos(
     ad_nusp varchar(9)
 )
-RETURNS TABLE (
-  codigo varchar(64),
-  nome varchar(255),
-  instituto varchar(128),
-  descricao text
-) AS $$
+RETURNS SETOF Curriculo AS $$
 BEGIN
     RETURN QUERY
-    SELECT Curriculo.codigo, Curriculo.nome, Curriculo.instituto, Curriculo.descricao FROM Curriculo
+    SELECT * FROM Curriculo
     WHERE Curriculo.ad_NUSP = get_curriculos.ad_nusp;
 END; $$  
 LANGUAGE plpgsql;
@@ -32,14 +27,10 @@ LANGUAGE plpgsql;
 CREATE OR REPLACE FUNCTION get_trilhas(
     cu_codigo varchar(64)
 )
-RETURNS TABLE (
-  codigo varchar(3),
-  nome varchar(64),
-  descricao text
-) AS $$
+RETURNS SETOF Trilha AS $$
 BEGIN
     RETURN QUERY
-    SELECT Trilha.codigo, Trilha.nome, Trilha.descricao FROM Trilha
+    SELECT * FROM Trilha
     WHERE Trilha.cu_codigo = get_trilhas.cu_codigo;
 END; $$  
 LANGUAGE plpgsql;
@@ -47,14 +38,10 @@ LANGUAGE plpgsql;
 CREATE OR REPLACE FUNCTION get_modulos(
     tr_codigo varchar(3)
 )
-RETURNS TABLE (
-  codigo varchar(3),
-  nome varchar(64),
-  descricao text
-) AS $$
+RETURNS SETOF Modulo AS $$
 BEGIN
     RETURN QUERY
-    SELECT Modulo.codigo, Modulo.nome, Modulo.descricao FROM Modulo
+    SELECT * FROM Modulo
     WHERE Modulo.tr_codigo = get_modulos.tr_codigo;
 END; $$  
 LANGUAGE plpgsql;
@@ -65,7 +52,7 @@ CREATE OR REPLACE FUNCTION get_disciplinas_do_modulo(
 RETURNS SETOF Disciplina AS $$
 BEGIN
     RETURN QUERY
-    SELECT Disciplina.codigo, Disciplina.nome, Disciplina.departamento, Disciplina.descricao, Disciplina.creditos FROM rel_dis_mod
+    SELECT Disciplina.* FROM rel_dis_mod
     INNER JOIN Disciplina on Disciplina.codigo = rel_dis_mod.di_codigo
     WHERE rel_dis_mod.mo_codigo = get_disciplinas_do_modulo.mo_codigo;
 END; $$  
@@ -77,7 +64,7 @@ CREATE OR REPLACE FUNCTION get_disciplinas_do_curriculo(
 RETURNS SETOF Disciplina AS $$
 BEGIN
     RETURN QUERY
-    SELECT d.codigo, d.nome, d.departamento, d.descricao, d.creditos
+    SELECT d.*
     FROM get_trilhas(cu_codigo) t, LATERAL get_modulos(t.codigo) m, LATERAL get_disciplinas_do_modulo(m.codigo) d;
 END; $$  
 LANGUAGE plpgsql;
