84aa3792d51ac1f73ea0888e710505dd250e9985
Gabriel Russo <gabrielrcarmo@gmail.com>
2019-07-01 13:51:54 -0300

    script sql rodando no container

--- ep3/acesso-pessoa.sql
+++ ep3/acesso-pessoa.sql
@@ -2,37 +2,7 @@ DROP DATABASE IF EXISTS acesso_pessoa;
 CREATE DATABASE acesso_pessoa;
 \c acesso_pessoa;
 
-CREATE EXTENSION postgres_fdw;
-
-CREATE SERVER acesso_server
-    FOREIGN DATA WRAPPER postgres_fdw
-    OPTIONS (host 'localhost', port '5432', dbname 'acesso');
-
-CREATE SERVER pessoa_server
-    FOREIGN DATA WRAPPER postgres_fdw
-    OPTIONS (host 'localhost', port '5432', dbname 'pessoa');
-
-CREATE FOREIGN TABLE pessoa_foreign (
-    id SERIAL NOT NULL,
-    nome text
-)
-SERVER pessoa_server
-OPTIONS (table_name 'pessoa');
-
-CREATE FOREIGN TABLE usuario_foreign (
-    id SERIAL NOT NULL,
-    nome text
-)
-SERVER acesso_server
-OPTIONS (table_name 'usuario');
-
-CREATE USER MAPPING FOR gabriel
-SERVER pessoa_server
-OPTIONS (user 'gabriel');
-
 CREATE TABLE rel_pes_usr (
     pes_id SERIAL NOT NULL,
-    usr_id SERIAL NOT NULL,
-    FOREIGN KEY (pes_id) REFERENCES pessoa_foreign (id),
-    FOREIGN KEY (usr_id) REFERENCES usuario_foreign (id)
+    usr_id SERIAL NOT NULL
 );
