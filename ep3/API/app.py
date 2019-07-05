84bbb23be6291b68c5247565540bc08d0051fd50
Victor Aliende da Matta <victormatta14@gmail.com>
2019-07-05 13:19:08 -0300

    remove people api

--- ep3/API/app.py
+++ ep3/API/app.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
 import access.api
-import people.api
 import curriculum.api
 import access_people.api
 import os
@@ -12,7 +11,6 @@ app.config['JWT_SECRET_KEY'] = os.environ['JWT_SECRET_KEY']
 jwt = JWTManager(app)
 
 app.register_blueprint(access.api.blueprint, url_prefix='/access')
-app.register_blueprint(people.api.blueprint, url_prefix='/people')
 app.register_blueprint(curriculum.api.blueprint, url_prefix='/curriculum')
 app.register_blueprint(access_people.api.blueprint, url_prefix='/access_people')
 
