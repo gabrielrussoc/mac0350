30978cd17520ef31e38f9b389c406d57997ca2af
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-07-06 16:52:40 -0300

    Define routes for the client app

--- ep3/client/src/App.tsx
+++ ep3/client/src/App.tsx
@@ -1,14 +1,19 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Home from './Home'
+import Curriculums from './Curriculums'
 import Login from './Login'
 
 const App = () => (
   <Router>
     <Fragment>
-      <Route exact path="/" component={Home} />
+      <Route exact path="/" component={Curriculums} />
       <Route path="/login" component={Login} />
+      <Route path="/cadastro" component={Login} />
+      <Route path="/curriculo/:curriculumId/disciplinas" component={Login} />
+      <Route path="/curriculo/:curriculumId/trilhas" component={Login} />
+      <Route path="/curriculo/:curriculumId/modulos" component={Login} />
+      <Route path="/modulo/:moduleId/disciplinas" component={Login} />
     </Fragment>
   </Router>
 );
