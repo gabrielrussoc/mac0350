678cb81255cb0d4ac6d1c4b5dc185d876b2202eb
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-07-06 16:53:04 -0300

    Crate first iteration for the Login

--- ep3/client/src/Login.tsx
+++ ep3/client/src/Login.tsx
@@ -1,9 +1,20 @@
 import React, { FunctionComponent } from 'react'
 
-const Home: FunctionComponent = () => (
-    <div className="App">
-        <p>This is the login screen</p>
+const Login: FunctionComponent = () => (
+  <div className="flex justify-center items-center bg-near-white vh-100">
+    <div className="bg-white br3 pv5 ph6">
+      <h1 className="tc">Login</h1>
+      <form className="mv5 flex flex-column">
+        <p>Usuário</p>
+        <input />
+        <p>Senha</p>
+        <input />
+        <button className="mv4 br-pill pv2">
+          Entrar
+        </button>
+      </form>
     </div>
+  </div>
 )
 
-export default Home
+export default Login
