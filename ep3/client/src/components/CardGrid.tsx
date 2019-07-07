ae3b9bee5890c5b5e5a28bdc6988bc2b68e4f5c2
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-07-07 02:11:04 -0300

    Make CardGrid component a bit more generic

--- ep3/client/src/components/CardGrid.tsx
+++ ep3/client/src/components/CardGrid.tsx
@@ -5,23 +5,19 @@ interface Props {
   content: {
     link: string
     text: string
+    optionText: string
+    optionLink: string
   }[]
 }
 
 const CardGrid: FunctionComponent<Props> = ({ content }) => (
-  <div className="flex flex-wrap justify-center items-center w-75 center pa4">
+  <div className="flex flex-wrap justify-center items-center w-70 center pa4">
     {content.map((item) => (
       <Fragment>
         <div className="flex flex-column justify-center items-center br3 bg-near-white pa4 w-80 ma4">
           <p>{item.text}</p>
-          <Link to={`${item.link}/trilhas`}>
-            <p>Trilhas</p>
-          </Link>
-          <Link to={`${item.link}/modulos`}>
-            <p>Módulos</p>
-          </Link>
-          <Link to={`${item.link}/disciplinas`}>
-            <p>Disciplinas</p>
+          <Link to={`${item.link}/${item.optionLink}`}>
+            <p>{item.optionText}</p>
           </Link>
         </div>
       </Fragment>
