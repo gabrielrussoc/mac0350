65b90ecb9ae499be8dee9404dfef3355e376d848
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-07-06 23:44:56 -0300

    Add CORS configuration to API

--- ep3/API/app.py
+++ ep3/API/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 import access.api
 import curriculum.api
 import access_people.api
@@ -14,7 +15,8 @@ app.register_blueprint(access.api.blueprint, url_prefix='/access')
 app.register_blueprint(curriculum.api.blueprint, url_prefix='/curriculum')
 app.register_blueprint(access_people.api.blueprint, url_prefix='/access_people')
 
+CORS(app)
 
 @app.route('/')
 def index():
-    return 'REACT'
+    return 'ROOT'
