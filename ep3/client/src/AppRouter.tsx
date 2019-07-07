518ca16406e6074521341eeb9b3475d1d36514d9
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-07-06 22:37:57 -0300

    Update components for each route in the client

--- ep3/client/src/AppRouter.tsx
+++ ep3/client/src/AppRouter.tsx
@@ -2,17 +2,23 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Curriculums from './Curriculums'
+import Home from './Home'
 import Login from './Login'
+import SignUp from './SignUp'
+import Disciplines from './Disciplines'
+import Tracks from './Tracks'
+import Modules from './Modules'
 
 const AppRouter = () => (
   <Router>
     <Fragment>
-      <Route exact path="/" component={Curriculums} />
+      <Route exact path="/" component={Home} />
       <Route path="/login" component={Login} />
-      <Route path="/cadastro" component={Login} />
-      <Route path="/curriculo/:curriculumId/disciplinas" component={Login} />
-      <Route path="/curriculo/:curriculumId/trilhas" component={Login} />
-      <Route path="/curriculo/:curriculumId/modulos" component={Login} />
+      <Route path="/cadastro" component={SignUp} />
+      <Route path="/curriculos" component={Curriculums} />
+      <Route path="/curriculo/:curriculumId/disciplinas" component={Disciplines} />
+      <Route path="/curriculo/:curriculumId/trilhas" component={Tracks} />
+      <Route path="/curriculo/:curriculumId/modulos" component={Modules} />
       <Route path="/modulo/:moduleId/disciplinas" component={Login} />
     </Fragment>
   </Router>
