07cc51bf76a8e7607e10f1aa4223310f9420ba02
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-07-06 23:45:55 -0300

    Update CardGrid

--- ep3/client/src/components/CardGrid.tsx
+++ ep3/client/src/components/CardGrid.tsx
@@ -12,26 +12,18 @@ const CardGrid: FunctionComponent<Props> = ({ content }) => (
   <div className="flex flex-wrap justify-center items-center w-75 center pa4">
     {content.map((item) => (
       <Fragment>
-        <Link to={item.link}>
-          <div className="flex justify-center items-center br3 bg-near-white pa4 w-80 ma4">
-            <p>{item.text}</p>
-          </div>
-        </Link>
-        <Link to={`${item.link}/disciplinas`}>
-          <div className="flex justify-center items-center br3 bg-near-white pa4 w-80 ma4">
-            <p>Disciplinas</p>
-          </div>
-        </Link>
-        <Link to={`${item.link}/modulos`}>
-          <div className="flex justify-center items-center br3 bg-near-white pa4 w-80 ma4">
-            <p>Módulos</p>
-          </div>
-        </Link>
-        <Link to={`${item.link}/trilhas`}>
-          <div className="flex justify-center items-center br3 bg-near-white pa4 w-80 ma4">
+        <div className="flex flex-column justify-center items-center br3 bg-near-white pa4 w-80 ma4">
+          <p>{item.text}</p>
+          <Link to={`${item.link}/trilhas`}>
             <p>Trilhas</p>
-          </div>
-        </Link>
+          </Link>
+          <Link to={`${item.link}/modulos`}>
+            <p>Módulos</p>
+          </Link>
+          <Link to={`${item.link}/disciplinas`}>
+            <p>Disciplinas</p>
+          </Link>
+        </div>
       </Fragment>
     ))}
   </div>
